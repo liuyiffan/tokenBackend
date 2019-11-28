38e82c38d8fe8b8a5390f9bd31a623cd0f90bdd5
liuyifan <liulyf@cn.ibm.com>
2019-11-29 01:31:04 +0800

    Simple log

--- src/main/java/com/ibm/handsup/controller/DJIController.java
+++ src/main/java/com/ibm/handsup/controller/DJIController.java
@@ -4,11 +4,13 @@ import com.ibm.handsup.entity.dji.Product;
 import com.ibm.handsup.entity.dji.Type;
 import com.ibm.handsup.response.Response;
 import com.ibm.handsup.service.DJIService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequestMapping("/dji")
 public class DJIController {
@@ -18,6 +20,7 @@ public class DJIController {
     @ResponseBody
     @RequestMapping(value="/types", method = RequestMethod.GET)
     public Response<List<Type>> getTypes() {
+        log.info("[DJI]Retrieve lists of type");
         return Response.ok(djiService.getAllTypes());
     }
 
@@ -26,6 +29,7 @@ public class DJIController {
     public Response<List<Product>> getProducts(
             @PathVariable final String typeId
     ) {
+        log.info("[DJI]Retrieve lists of product");
         return Response.ok(djiService.getProductsByType(typeId));
     }
 }
