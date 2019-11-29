475f50682fbf4b4a6472071dc29eea3407b8a872
liuyifan <liulyf@cn.ibm.com>
2019-11-29 10:16:39 +0800

    ssl of aliyun

--- src/main/java/com/ibm/handsup/Application.java
+++ src/main/java/com/ibm/handsup/Application.java
@@ -16,30 +16,30 @@ public class Application {
         SpringApplication.run(Application.class, args);
     }
 
-//    @Bean
-//    public Connector connector(){
-//        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
-//        connector.setScheme("http");
-//        connector.setPort(8080);
-//        connector.setSecure(false);
-//        connector.setRedirectPort(8443);
-//        return connector;
-//    }
-//
-//    @Bean
-//    public TomcatServletWebServerFactory tomcatServletWebServerFactory(Connector connector){
-//        TomcatServletWebServerFactory tomcat=new TomcatServletWebServerFactory(){
-//            @Override
-//            protected void postProcessContext(Context context) {
-//                SecurityConstraint securityConstraint=new SecurityConstraint();
-//                securityConstraint.setUserConstraint("CONFIDENTIAL");
-//                SecurityCollection collection=new SecurityCollection();
-//                collection.addPattern("/*");
-//                securityConstraint.addCollection(collection);
-//                context.addConstraint(securityConstraint);
-//            }
-//        };
-//        tomcat.addAdditionalTomcatConnectors(connector);
-//        return tomcat;
-//    }
+    @Bean
+    public Connector connector(){
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setScheme("http");
+        connector.setPort(8080);
+        connector.setSecure(false);
+        connector.setRedirectPort(8443);
+        return connector;
+    }
+
+    @Bean
+    public TomcatServletWebServerFactory tomcatServletWebServerFactory(Connector connector){
+        TomcatServletWebServerFactory tomcat=new TomcatServletWebServerFactory(){
+            @Override
+            protected void postProcessContext(Context context) {
+                SecurityConstraint securityConstraint=new SecurityConstraint();
+                securityConstraint.setUserConstraint("CONFIDENTIAL");
+                SecurityCollection collection=new SecurityCollection();
+                collection.addPattern("/*");
+                securityConstraint.addCollection(collection);
+                context.addConstraint(securityConstraint);
+            }
+        };
+        tomcat.addAdditionalTomcatConnectors(connector);
+        return tomcat;
+    }
 }
